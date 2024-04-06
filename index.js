726e7bbba57906bc3365afdaaf9e8c4962237ca9
Edson Yamble <edsonyamble2@gmail.com>
2024-04-06 19:31:20 +0700

    js это динамическая типизация потому что можно меня типи перемен потом без проблем

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const reneMilk = {
-	you: 'lait blanc',
-	asa: 10,
-	aus: true,
-}
-const dadoMilk = reneMilk
-console.table(dadoMilk)
-dadoMilk.asa = 1000
-console.log(dadoMilk.asa)
-reneMilk.asus = 'netbook'
-console.table(dadoMilk)
-delete dadoMilk.you
-console.log(dadoMilk.you)
-console.log(dadoMilk)
+// const reneMilk = {
+// 	you: 'lait blanc',
+// 	asa: 10,
+// 	aus: true,
+// }
+// const dadoMilk = reneMilk
+// console.table(dadoMilk)
+// dadoMilk.asa = 1000
+// console.log(dadoMilk.asa)
+// reneMilk.asus = 'netbook'
+// console.table(dadoMilk)
+// delete dadoMilk.you
+// console.log(dadoMilk.you)
+// console.log(dadoMilk)
+let a = 10 
+a = true 
+a = 'edson'
+a = null 
+a = undefined 
+a = Symbol 
+console.log(a);
