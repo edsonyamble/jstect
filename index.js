0700821e8e4c4b919408e1b42c01ede13bf173f8
Edson Yamble <edsonyamble2@gmail.com>
2024-04-08 12:36:21 +0700

    новый способ получить менять данных из обект

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// function a() {
-//   console.log('this you')
-// }
-// a() //this you
-// a = 10 //a =10 variable 
-// a()// error a don t function because a is variables 
-
-const b = () => {
-  console.log('you are ))')
-}
-//permet de ne pas changer la variable qui est ici la fonction pour eviter les probleme de chamgement de function ou variable 
-b()//permet de voir la valeur de la function 
-b = 90// b de une variable mais a cause de const dns la function il yaurau erreur que on ne pas chamger la function a cause de const 
+const myCity = {city : 'california'}
+//add value in object 
+// myCity.popular = 'true'
+myCity['popular'] = 'true'
+console.log(myCity)
+const countryPropertyName = 'country'
+myCity[countryPropertyName] = 'USA'
+console.log(myCity)
