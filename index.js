7d173ecd783fb47d92ad0c3b75843059a37ceaad
Edson Yamble <edsonyamble2@gmail.com>
2024-04-09 12:07:28 +0700

    change

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-console.log(3 + 2);
 
-console.table(["edson", "rene"]);
-a = 10;
-a++;
-console.log(a);
-
-обьявление переменых 
-let const var 
-let and var вышли с  esmascript 6 var не рекомендуеться исползует раньше только он исползовали 
-let a обявление переменых
-const b = 5 обфвление переменн и присваивание 
-a = true присваивание от  let  a 
