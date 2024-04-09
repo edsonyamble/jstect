1d1400f277b9666a5ecadc1d6861f4ccc90e8f38
Edson Yamble <edsonyamble2@gmail.com>
2024-04-09 14:05:58 +0700

    function call

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const myCity = {
   city: "New York",
   cityGreeting: function () {
-    console.log("Greet;;");//здесть не можем const  внутри  const   вызвать поэтому тут исползуем только function () 
+    console.log("Greet;;"); //здесть не можем const  внутри  const   вызвать поэтому тут исползуем только function ()
   },
 };
-myCity.cityGreeting()// вызвать функции 
-метод или функции внутри обект 
+myCity.cityGreeting(); // вызвать функции
+edson = () => {
+  return console.log(10);
+};
+edson();//вызов функции 
