63ec5e70741fe7e8960225b71294516556986609
Edson Yamble <edsonyamble2@gmail.com>
2024-04-10 16:52:23 +0700

    json convert json line on object javascritp and convert jbject in json

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const myCity = {
-  city: "New York",
-  cityGreeting () {
-    console.log("Greet;;");//здесть не можем const  внутри  const   вызвать поэтому тут исползуем только function () но можеи без function
-  },
-};
-myCity.cityGreeting()// вызвать функции 
+// JSON.parse() //это чтобы конвертивировать строка джейсон в обект джаваскрипт
+// JSON.stringify()//это чтобы конвертивировать обект джавасерипт  на строка джейсон
+const posts = {
+	title: 'Edson',
+	likeQty: 5,
+}
+JSON.stringify(posts) //получение формат строка джейсон
+const stringgo = JSON.stringify(posts)
+JSON.parse(stringgo)//получение формат обект джаваскрипт 
+
+
+
+//эти методы работает на сервере или прям в консоле браузере 
