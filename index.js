9026502558676cedbf28f147f9e6c7f722d646a7
Edson Yamble <edsonyamble2@gmail.com>
2024-04-15 14:33:07 +0700

    call back function this function in function

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const personOne = {
-	name: 'edson',
-	age: 22,
-}
-function increasePersonAge(person) {
-  const inperF = {...person}
-	inperF.age += 1
-	return inperF
-}
-const inperF = increasePersonAge(personOne)
-console.log(inperF)
-console.log(personOne.age)
+function anotherffunction() {
 
+}
+function fnwhithCallback(callBackfunction){
+callBackfunction()
+}
+fnwhithCallback(anotherffunction) 
