3328b8c1d283f233f7411ab07327f742751a42f0
Edson Yamble <edsonyamble2@gmail.com>
2024-04-25 11:40:43 +0700

    ложные значение проверка

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-ложные значение 
-false 
-0 
-''
-undefined
-null 
+// ложные значение  провека
+
+// console.log(Boolean(false))
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean('') пустая страка 
+// console.log(Boolean(null))
+// получаем false 
+
+
+// console.log(Boolean(' ') непустая страка 
+получаем true это не ложная значение 
