2eabf4aaa1f9795b7ce9f67aba96f4c62e2139c6
Edson Yamble <edsonyamble2@gmail.com>
2024-04-25 12:01:33 +0700

    ложные записы через 2 no

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// ложные значение  провека
+проверка ложные запись через !!
 
-// console.log(Boolean(false))
-// console.log(Boolean(0))
-// console.log(Boolean(undefined))
-// console.log(Boolean('') пустая страка 
-// console.log(Boolean(null))
-// получаем false 
-
-
-// console.log(Boolean(' ') непустая страка 
-получаем true это не ложная значение 
+!!0 //false
+!!false //false
+!!undefined//false
+!!null//false
+!!''//false
