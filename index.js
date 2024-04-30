f13598fd672270e903c8f784e4a5d9a85fa9e2bf
Edson Yamble <edsonyamble2@gmail.com>
2024-04-30 07:43:35 +0700

    template string whith concatenation

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// конкетенации строк 
-// это сездинить два строка 
-const hello = 'Hello'
-const word = 'word'
-const finish = hello +' '+word
-console.log(finish)
-console.log('hello '+'world')
+// шаблоны строка 
+// позволяет написать конкентациа строка боле коротким 
+ const hello = 'hello'
+ const word = 'word'
+ const finish = `${hello} ${word}`
+ console.log(finish)
+
+
+ const hey = 'Привет'
+ const name = 'меня зовут Эдсон'
+ const country = 'я из кот дивуар'
+ const personality = `${hello},${name} ${country}`
+ console.log(personality)
