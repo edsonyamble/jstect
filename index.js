b82a1ea46aff5d2b2753dd78e128992035f75ad3
Edson Yamble <edsonyamble2@gmail.com>
2024-05-12 11:42:26 +0700

    default function value paramet add tree point or delete  example

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-//default parameters function values 
-function multByFactor(value, multiplier = 1){
-  return value * multiplier
-}
-multByFactor(5, 2)//20 value = 5 and multiplier = 2
-multByFactor(5)//5 value = 5 and multiplier = default 1
+const newPost = (post,addDate =Date()) => ({
+ ...post,addDate
+})//avec les trois point pour la function en creant objet on met les parenthese 
+//mais on peut supprimmer les parenthese comme 
+//une fonction normale mais pour cela 
+//on enleve les trois points 
+
+const newPostTwo = (post, addDate = Date()) => {
+  return post,addDate
+}//ici on retire  les troit point et les  parenthese et  on ajoute retourne
