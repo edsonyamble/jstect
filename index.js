a6a92891a302ebbac9417c3e6d1ae834325561aa
Edson Yamble <edsonyamble2@gmail.com>
2024-05-12 14:11:13 +0700

    call error in javascript

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const newPost = (post,addDate =Date()) => ({
- ...post,addDate
-})//avec les trois point pour la function en creant objet on met les parenthese 
-//mais on peut supprimmer les parenthese comme 
-//une fonction normale mais pour cela 
-//on enleve les trois points 
-
-const newPostTwo = (post, addDate = Date()) => {
-  return post,addDate
-}//ici on retire  les troit point et les  parenthese et  on ajoute retourne
+const fnWithError = () => {
+throw new Error ('Error some')//call error 
+}
+fnWithError()//Error some
+console.log('continue...')
