66e931edbaa21ab78ff620fbf3a6c816098a5be2
Edson Yamble <edsonyamble2@gmail.com>
2024-05-12 10:26:14 +0700

    arrrow function or стрелочная функция

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-let callBack = function(a, b){
-  let c
-  a = a + 1
-  c = a + b
-  return c
-  }
-console.log(callBack(5, 9))
-  //call function expression  with variable  
-
-
+//arrow function
+() => {
+  console.log('edson')
+}
