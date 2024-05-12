6a1df5605c03d0496a52513989df8eb9161ab80b
Edson Yamble <edsonyamble2@gmail.com>
2024-05-12 11:28:46 +0700

    default parametres function value

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const newPost = (post, addDate = Date()) => ({
-	...post,
-	addDate,
-})
-const firstPost = {
-	id: 1,
-  author:'Edson'
+//default parameters function values 
+function multByFactor(value, multiplier = 1){
+  return value * multiplier
 }
-const secondPost = {
-	id: 2,
-	author: 'Kcenia',
-}
-console.dir(newPost(firstPost))
-console.dir(newPost(secondPost))
-//default function parametr values example
+multByFactor(5, 2)//20 value = 5 and multiplier = 2
+multByFactor(5)//5 value = 5 and multiplier = default 1
