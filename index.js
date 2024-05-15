534bd417431f76c4808206e14ff3eae2ff0a6f83
Edson Yamble <edsonyamble2@gmail.com>
2024-05-15 13:54:34 +0700

    term and condition if else if if

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-//dexturization  fuction
-const userProfile = {
-	name: 'Edson',
-	commentQty: 23,
-	haSigned: true,
+//условия инструкция terms and condition
+// if  if else switch тенарный опаратор
+//if exemple
+let val = 10
+if (val > 5) {
+	val += 20
 }
-const userInfo = ({ name, commentQty }) => {
-	if (!commentQty) {
-		return `User ${name} has comment`
-	} else return `User ${name} has ${commentQty} comment`
+console.log(val) //30
+//if else
+let valOne = 10
+if (valOne < 5) {
+	valOne += 20
+} else {
+	valOne -= 30
 }
-console.log(userInfo(userProfile))
+console.log(valOne) //-20
+//if else if else
+let age = 25
+if (age >18){
+	console.log('it s adult')
+}
+else if (age >= 12 ) {
+	console.log('it s teenager')
+}
+else {console.log('it s child')}
