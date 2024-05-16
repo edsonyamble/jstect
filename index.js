30e0929200ffdd51041e4c541d7c844b1860d732
Edson Yamble <edsonyamble2@gmail.com>
2024-05-16 11:31:49 +0700

    ternary operator

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-//terms and condition switch
-const month = 2
+//terms and condition тенарный оператор ternary operator 3 операнд 
 
-switch (month) {
-	case 12:
-		console.log('декабрь')
-		break //sans lui les conditions continu si c est vrai
-	case 1:
-		console.log('Январь')
-		break //sans lui les conditions continu si c est vrai
-	case 2:
-		console.log('Февраль')
-		break //sans lui les conditions continu si c est vrai
-	default:
-		console.log('это не зимний месяц')
+//условия ? выражение1 (if ) : выражение 2 (else)
+let value = 11 
+value === 11 ? console.log('все хорошо') : console.log('Falssse') 
+// or 
+// value === 11
+// ?console.log('все хорошо')
+// :console.log('Falssse')
+const value1 = 11
+const value2 = 23 
+try {value1  && value2 ? myFunction(value1,value2) : myFunction()}
+catch (error) {
+	console.error(error)
+	console.log(error.message)
 }
+
+
+let value3 = 11
+console.log(value3 >= 0 ? value3 : -value3)
+value3 = -5 
+console.log(value3 >= 0 ? value3 : -value3)
