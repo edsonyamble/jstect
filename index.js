0fff1966eca4d8750759c254a16388422baf47e1
Edson Yamble <edsonyamble2@gmail.com>
2024-05-17 10:37:47 +0700

    cycles and cycle for

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-//terms and condition тенарный оператор ternary operator 3 операнд 
-
-//условия ? выражение1 (if ) : выражение 2 (else)
-let value = 11 
-value === 11 ? console.log('все хорошо') : console.log('Falssse') 
-// or 
-// value === 11
-// ?console.log('все хорошо')
-// :console.log('Falssse')
-const value1 = 11
-const value2 = 23 
-try {value1  && value2 ? myFunction(value1,value2) : myFunction()}
-catch (error) {
-	console.error(error)
-	console.log(error.message)
+//циклов cycles  это инструкциия
+//for for ..in.. while do..while for..of
+// for (начало инструкция,условие,итерационное действие) {
+// //...,блок кода
+// }
+for (let i = 0; i < 5; i++) {
+	console.log(i)
 }
-
-
-let value3 = 11
-console.log(value3 >= 0 ? value3 : -value3)
-value3 = -5 
-console.log(value3 >= 0 ? value3 : -value3)
