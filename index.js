74a3b3eb186abb56bb2d0a2cecb614686f7f5b90
Edson Yamble <edsonyamble2@gmail.com>
2024-05-24 11:59:07 +0700

    async await

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-//промисс позволяет обрабатывать отложение во время
-//события
-//промис это обещание предоставить результат позже
-//промисс может вернут ошибку если резултать не возможно
-//состояне промисс ожидание исполен отклонен
-const getData = url =>
-	new Promise((resolve, reject) =>
-		fetch(url)
-			.then(response => response.json())
-			.then(json => resolve(json))
-			.catch(error => reject(error))
-	)
-getData('https://jsonplaceholder.typicode.com/todos')
-	.then(data => console.log(data))
-	.catch(error => console.error(error))
+//ASync/await это синтакс дляя упршение работы с промисси
+async function asyncFn() {
+	//return промисс
+}
+const asynFn = async () => {
+	//return промисс
+}
