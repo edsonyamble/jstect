3bef87bfde78a1f046809f76fec8545cbc391c55
Edson Yamble <edsonyamble2@gmail.com>
2024-05-24 16:30:52 +0700

    async await try catch

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-
-const getData = async url =>{
-		const res = await fetch(url)
-		const json = await res.json()
-		return json 
+const getData = async url => {
+	const res = await fetch(url)
+	const json = await res.json()
+	return json
 }
-	
-getData('https://jsonplaceholder.typicode.com/todos')
-	.then(data => console.log(data))
-	.catch(error => console.error(error))
+const url = 'https://jsonplaceholder.typicode.com/todos'
+try {
+	const data = await getData(url)
+	console.log(data)
+} catch (error) {
+	console.log(error.message)
+}
