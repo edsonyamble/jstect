3d6ef1b9ad9d50b24c32182658afa26bb798a047
Edson Yamble <edsonyamble2@gmail.com>
2024-06-14 13:12:16 +0700

    split file

--- d/app_01.js
+++ d/app_01.js
@@ -4,3 +4,6 @@ const pathToFile = './info.txt'
 const data = fs.readFileSync(pathToFile, { encoding: 'utf8', flag: 'r' })
 console.log(data)
 //fs.readFileSync(путь, параметры)
+//split the file line by line
+let dataArr = data.split('\n') //permet de metre a la ligne
+console.log(dataArr)
