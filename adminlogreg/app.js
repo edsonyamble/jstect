b18b90f4db0e7bedb00cc7eccdfc0346055699e0
Edson Yamble <edsonyamble2@gmail.com>
2024-06-17 08:56:04 +0700

    test admin log 1

---
+++ adminlogreg/app.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const users = require("./users");
+const mimeTypes = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".JPEG": "image/jpeg",
+  ".ico": "image/ico",
+  ".gif": "image/gif",
+  ".svg": "image/svg",
+  ".wav": "audio/wav",
+  ".mp3": "audio/mp3",
+  ".mp4": "video/mp4",
+  ".woff": "application/font-woff",
+  ".ttf": "application/font-ttf",
+  ".eot": "application/vnd.ms-fontobject",
+  ".otf": "application/otf",
+  ".wasm": "application/wasm",
+};
+function staticFile(res, filePath, ext) {
+  res.setHeader("content-type", mimeTypes[ext]);
+  fs.readFile(
+    "./public" + filePath,
+    // { encoding: 'utf-8', flag: 'r' },
+    (error, data) => {
+      if (error) {
+        res.statusCode = 404;
+        res.end();
+      }
+      res.end(data);
+    }
+  );
+}
+http
+  .createServer(function (req, res) {
+    let url = req.url;
+    console.log(url);
+    switch (url) {
+      case "/":
+        console.log("main page");
+        staticFile(res, "/html/main.html", ".html");
+        break;
+      default:
+        const extname = String(path.extname(url)).toLocaleLowerCase();
+        if (extname in mimeTypes) staticFile(res, url, extname);
+        else {
+          res.statusCode = 404;
+          res.end();
+        }
+    }
+  })
+  .listen(3500);
---
+++ adminlogreg/users.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "9c05b0d586377d5c": {
+    name: "matador",
+    pass: "123321",
+  },
+  "8c05b0d586377d4f": {
+    name: "roketa",
+    pass: "bcheat",
+  },
+};
