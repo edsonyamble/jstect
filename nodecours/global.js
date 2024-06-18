721725a13de2d11efcbac7158da48eea29b56ac1
Edson Yamble <edsonyamble2@gmail.com>
2024-06-18 12:45:39 +0700

    global

---
+++ nodecours/global.js
@@ -0,0 +1,27 @@
+// console.log(global);
+//setTimeout(func|code, [delay], [arg1], [arg2], ...);
+// setTimeout(
+//   (nam) => {
+//     console.log(`Hello ${nam}`);
+//   },
+//   2000,
+//   "Edson"
+// );
+// //setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+// // повторить с интервалом 2 секунды
+// let timerId = setInterval(() => alert('tick'), 2000);
+
+// // остановить вывод через 5 секунд
+// setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+// console.log(__dirname);//путь до файл
+// console.log(__filename);//путь до файл и имя и расширение
+// console.log(process)
+// console.log(process.env);
+// node global 1 Edson [1,2,3]
+// console.log(process.argv);
+// console.log(`hello ${process.argv[2]}`);//node global edson = hello edson
+// const url = new URL("https://webDev.com/path/name#test");
+// console.log(url.hostname);
+// console.log(url.href);
+// console.log(url.pathname);
+// console.log(url.hash);
