b6e6f649bfc8a7cbce6479756a2b7b9518f7c2b4
Edson Yamble <edsonyamble2@gmail.com>
2024-06-20 12:15:19 +0700

    create server whith json

---
+++ nodecours/servercreate/app.js
@@ -0,0 +1,24 @@
+const http = require('http')
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+	console.log('Server request')
+
+	res.setHeader('Content-Type', 'application/json')
+
+	const data = JSON.stringify([
+		{ name: 'Tommy', age: 35 },
+		{ name: 'Arthur', age: 40 },
+	])
+
+	// res.write('<head><link rel="stylesheet" href="#"></head>');
+	// res.write('<h1>Hello world!</h1>');
+	// res.write('<p>My name is Yauhen</p>');
+
+	res.end(data)
+})
+
+server.listen(PORT, 'localhost', error => {
+	error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
