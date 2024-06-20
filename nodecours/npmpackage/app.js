8a14ecedb32205e0efdd25ec653bc8b86b52d060
Edson Yamble <edsonyamble2@gmail.com>
2024-06-20 13:16:50 +0700

    npmpackage

---
+++ nodecours/npmpackage/app.js
@@ -0,0 +1,57 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+	console.log('Server request')
+	console.log('edson')
+	console.log('edson yamble')
+
+	res.setHeader('Content-Type', 'text/html')
+	//if(req.url='/'){
+	//fs.readFile('./views/index.html',(err,data){
+	// if(err){console.log(err)res.end()}
+	//  else res.write(data) res.end() })
+	//}
+	const createPath = page => path.resolve(__dirname, 'views', `${page}.html`)
+	let basePath = ''
+
+	switch (req.url) {
+		case '/':
+		case '/home':
+		case '/index.html':
+			basePath = createPath('index')
+			res.statusCode = 200
+			break
+		case '/about-us':
+			res.statusCode = 301
+			res.setHeader('Location', '/contacts')
+			res.end()
+			break //redirect contact page
+		case '/contacts':
+			basePath = createPath('contacts')
+			res.statusCode = 200
+			break
+		default:
+			basePath = createPath('error')
+			res.statusCode = 404
+			break
+	}
+
+	fs.readFile(basePath, (err, data) => {
+		if (err) {
+			console.log(err)
+			res.statusCode = 500
+			res.end()
+		} else {
+			res.write(data)
+			res.end()
+		}
+	})
+})
+
+server.listen(PORT, 'localhost', error => {
+	error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
